b6f48699eaafc88e9617d2a871db7144dc05a2bd
nisarg-bhuva <nisarg1895@gmail.com>
2025-05-07 09:06:59 +0530

    feat(service): add service functionality

---
+++ app/controllers/services_controller.rb
@@ -0,0 +1,47 @@
+class ServicesController < ApplicationController
+  before_action :set_service, only: [ :show, :edit, :update, :destroy ]
+
+  def new
+    @service = Service.new
+    @services = Service.all
+  end
+
+  def create
+    @service = Service.new(service_params)
+    if @service.save
+      redirect_to new_service_path, notice: "Service was successfully created."
+    else
+      @services = Service.all
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @service.update(service_params)
+      redirect_to new_service_path, notice: "Service was successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  def show
+  end
+
+  def destroy
+    @service.destroy
+    redirect_to new_service_path, notice: "Service was successfully destroyed."
+  end
+
+  private
+
+  def set_service
+    @service = Service.find(params[:id])
+  end
+
+  def service_params
+    params.require(:service).permit(:name, :price)
+  end
+end
---
+++ app/models/service.rb
@@ -0,0 +1,4 @@
+class Service < ApplicationRecord
+  validates :name, presence: true
+  validates :price, numericality: { greater_than: 0 }
+end
